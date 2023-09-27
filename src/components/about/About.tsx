bb989fa3464bce29ae6ce17d943e3fdaf173614c
vspeed21 <vtorresquintanilla0206@gmail.com>
2023-09-26 20:03:10 -0600

    update: about info; add bg-pattern

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,10 +1,10 @@
 
 function About() {
   return (
-    <div className="bg-[#DDDDDD] py-4 md:py-10 px-2 md:px-0 mb-16" id="nosotros">
-      <div className="flex flex-col gap-4 justify-center items-center md:max-w-lg md:mx-auto">
-        <h2 className="font-bold text-2xl md:text-3xl">Acerca de</h2>
-        <p className="text-center md:text-lg">
+    <div className="bg-hero-about bg-cover bg-center bg-no-repeat py-4 md:py-10 px-2 md:px-0 mb-16" id="nosotros">
+      <div className="flex flex-col gap-4 justify-center items-center md:max-w-xl md:mx-auto">
+        <h2 className="font-bold text-2xl md:text-3xl text-white">Acerca de</h2>
+        <p className="text-center md:text-lg text-gray-100">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, laboriosam quam eius harum quos reprehenderit dolores incidunt quo fugit. Saepe asperiores
         </p>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,10 @@ export default {
     plugin(function ({ addComponents }) {
       addComponents({
         '.bg-hero-pattern': {
-          backgroundImage: "linear-gradient(to right, rgb(0 0 0 /.7) 50%, rgb(0 0 0 /.9) 100%), url('img/header.jpg')",
+          backgroundImage: "linear-gradient(to right, rgb(0 0 0 /.6) 80%, rgb(0 0 0 /.6) 100%), url('img/header.jpg')",
+        },
+        '.bg-hero-about': {
+          backgroundImage: "linear-gradient(to right, rgb(0 0 0 /.6) 80%, rgb(0 0 0 /.6) 100%), url('img/acerca de.jpg')",
         },
       })
     }),
