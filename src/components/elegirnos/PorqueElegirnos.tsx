dc0427e29348e07b98c013a9d366f1423e99c718
vspeed21 <vtorresquintanilla0206@gmail.com>
2023-09-26 20:06:01 -0600

    update: porqueElegirnos info & styles

--- src/components/elegirnos/PorqueElegirnos.tsx
+++ src/components/elegirnos/PorqueElegirnos.tsx
@@ -6,10 +6,10 @@ function PorqueElegirnos() {
       <Heading title="¿Porque elegirnos?" />
 
       <div className="flex flex-col gap-4 justify-center items-center md:flex-row max-w-3xl mx-auto w-[90%] mb-10">
-        <p className="-mt-3 md:-mt-0 text-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt fuga vero beatae accusamus blanditiis, aliquid omnis quaerat doloremque delectus exercitationem
+        <p className="-mt-3 md:-mt-0 text-center md:text-start text-lg">
+          Nos comprometemos a brindar un servicio personalizado para satisfacer las necesidades de cualquier empresa, desde pequeñas empresas hasta grandes corporaciones
         </p>
-        <img className="w-80" src="https://placehold.co/600x400" />
+        <img className="w-80" src="img/porque elejirnos.jpg" />
       </div>
       <hr />
     </div>
