2bf8b7c4d00dccfd1694888ca1a9f0983f1916e2
vspeed21 <vtorresquintanilla0206@gmail.com>
2023-09-26 19:08:14 -0600

    add: bg-hero-pattern class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import plugin from 'tailwindcss/plugin';
 export default {
   content: [
     'index.html',
@@ -7,6 +8,14 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        '.bg-hero-pattern': {
+          backgroundImage: "linear-gradient(to right, rgb(0 0 0 /.7) 50%, rgb(0 0 0 /.9) 100%), url('img/header.jpg')",
+        },
+      })
+    }),
+  ],
 }
 
