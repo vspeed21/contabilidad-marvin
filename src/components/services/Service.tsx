3534207fecb0f5606b45bc165fb4d6736969d243
vspeed21 <vtorresquintanilla0206@gmail.com>
2023-09-27 20:29:16 -0600

    update: img tag to CustomImage component

--- src/components/services/Service.tsx
+++ src/components/services/Service.tsx
@@ -1,4 +1,5 @@
 import { IService } from "../shared/interfaces"
+import { CustomImage } from '../shared/index';
 
 interface Props {
   service: IService
@@ -7,7 +8,7 @@ interface Props {
 function Service({ service }: Props) {
   return (
     <div key={service.title} className="sm:w-[30%] border rounded-md">
-      <img src={service.imageUrl} alt={`image ${service.title}`} className="w-full md:h-44 lg:h-48" />
+      <CustomImage imageName={service.imageName} type={service.imageType} className="w-full md:h-44 lg:h-48" />
       <div className="py-1 px-2">
         <h3 className="text-center text-xl font-bold pt-2 pb-4">{service.title}</h3>
         <p className="mb-4 text-center md:text-start">{service.caption}</p>
--- src/lib/constants/Servicios.ts
+++ src/lib/constants/Servicios.ts
@@ -5,16 +5,22 @@ const services: IService[] = [
     imageUrl: 'img/estados financieros.jpeg',
     title: 'Estados financieros',
     caption: 'Conoce la salud financiera de tu empresa y toma desiciones informadas.',
+    imageType: 'jpeg',
+    imageName: 'estados-financieros',
   },
   {
     imageUrl: 'img/declaracion impuestos.jpeg',
     title: 'Declaración de impuestos',
     caption: 'Presenta tus impuestos sin complicaciones y ahorra tiempo y dinero.',
+    imageType: 'jpeg',
+    imageName: 'declaracion-impuestos',
   },
   {
     imageUrl: 'img/registros contables.jpg',
     title: 'Registros contables',
     caption: 'Controla tus finanzas de forma eficiente y toma desiciones acertadas',
+    imageType: 'jpg',
+    imageName: 'registros-contables',
   },
 ];
 
